91d02cffc5a94ee8b1cd9a12608bae921dd5d55f
kossimovv <abdulhafizqosimov1151@gmail.com>
2023-07-13 18:13:21 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+var USD = (10900)
+var EURO = (12272.64)
+
+var planeTicket = (500 * USD)
+var hotelPrice = (250 * USD)
+var travelPrice = (120 * EURO)
+var amount = (airplaneBlet + hotel + travel)
+var userValuta = prompt("Sumani kiriting")
+
+if (amount <= userValuta) {
+  alert("Oq yo'l, Alisher!")
+  console.log("Oq yo'l, Alisher!")
+} else {
+  alert("Alisher, ozgina sabr qilish kerak bo'lar ekan.")
+  console.log("Alisher, ozgina sabr qilish kerak bo'lar ekan.")
+}
